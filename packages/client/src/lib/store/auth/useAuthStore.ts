387b0ea2db7f6760946861b710d68cd31d03f118
Dok6n <ehrbs2018@gmail.com>
2022-12-14 21:44:54 +0900

    feat: implements subscribe to current user state

--- packages/client/src/lib/store/auth/useAuthStore.ts
+++ packages/client/src/lib/store/auth/useAuthStore.ts
@@ -1,5 +1,5 @@
 import create from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
+import { devtools, subscribeWithSelector } from 'zustand/middleware';
 import { signInWithPopup, User } from 'firebase/auth';
 import { auth } from '../../firebase/firebaseClient';
 import { Provider } from '../../firebase/types';
@@ -13,18 +13,26 @@ interface AuthStore {
 }
 
 export const useAuthStore = create<AuthStore>()(
-  devtools((set, get) => ({
-    userState: userStorage.get(),
-    login: async (provider) => {
-      const { user } = await signInWithPopup(auth, provider);
-      return set(({ userState }) => ({
-        userState: { ...user },
-      }));
-    },
-    logout: async () => {
-      await auth.signOut();
-      return set(({ userState }) => ({ userState: null }));
-    },
-    setUserState: (newState) => set(() => ({ userState: { ...newState } })),
-  })),
+  devtools(
+    subscribeWithSelector((set, get) => ({
+      userState: userStorage.get(),
+      login: async (provider) => {
+        const { user } = await signInWithPopup(auth, provider);
+        return set(({ userState }) => ({
+          userState: { ...user },
+        }));
+      },
+      logout: async () => {
+        await auth.signOut();
+        return set(({ userState }) => ({ userState: null }));
+      },
+      setUserState: (newState) => set(() => ({ userState: { ...newState } })),
+    })),
+  ),
+);
+
+useAuthStore.subscribe(
+  (state) => state.userState,
+  (previousSelectedState) =>
+    console.log('[subscribe]has user -', previousSelectedState ? true : false),
 );
