68a01243b6366a2ed98287fd4e9a82b06f9f24d1
Dok6n <ehrbs2018@gmail.com>
2022-12-16 04:00:09 +0900

    feat: update graphql codegen config

--- codegen.ts
+++ codegen.ts
@@ -2,19 +2,26 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   schema: 'packages/server/src/**/*.gql',
-  documents: 'packages/client/src/**/*.graphql',
+  documents: [
+    'packages/client/src/**/*.{ts,graphql}',
+    'packages/client/src/**/*.generated.ts',
+  ],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
     'packages/client/src/lib/graphql/query/': {
       preset: 'near-operation-file',
       presetConfig: {
-        extension: '.graphql.tsx',
+        extension: '.generated.tsx',
         baseTypesPath: '../types.tsx',
       },
       plugins: ['typescript-operations', 'typescript-react-apollo'],
+      config: {
+        withHooks: true,
+        withRefetchFn: true,
+      },
     },
     'packages/client/src/lib/graphql/types.ts': {
-      plugins: ['typescript', 'typescript-operations'],
+      plugins: ['typescript'],
     },
     'packages/server/src/generated/graphql.ts': {
       plugins: ['typescript', 'typescript-resolvers'],
