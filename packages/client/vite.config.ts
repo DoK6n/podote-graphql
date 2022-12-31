36cbe59582aeb9aaad5e9eb2bb7ab6b9655a6e86
Dok6n <ehrbs2018@gmail.com>
2023-01-01 01:52:55 +0900

    chore: add vite pwa config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from '@svgr/rollup';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,41 @@ export default defineConfig({
       },
     }),
     svgr(),
+    VitePWA({
+      injectRegister: 'auto', // 서비스 작업자를 자동으로 등록합니다
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['logo.svg'],
+      manifest: {
+        name: 'Podote App',
+        short_name: 'Podote',
+        description: 'Purple TodoList Note App',
+        theme_color: '#3b305a',
+        icons: [
+          {
+            src: 'logo.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: '/logo.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'logo.svg', // <== don't add slash, for testing
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
   ],
   server: {
     host: true,
