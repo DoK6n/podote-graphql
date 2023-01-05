c2d97a12f107eec978841715fbca6ee1b8350597
Dok6n <ehrbs2018@gmail.com>
2023-01-06 01:19:45 +0900

    design: add props to resize svg

--- packages/client/src/components/base/IconButton.tsx
+++ packages/client/src/components/base/IconButton.tsx
@@ -1,7 +1,8 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { colors } from '../../styles/colors';
 
-export const IconButton = styled.span`
+export const IconButton = styled.span<{ svgSize?: number }>`
   cursor: pointer;
   border: 1px solid #0000;
   border-radius: 3px;
@@ -9,7 +10,14 @@ export const IconButton = styled.span`
   color: ${colors.purple0};
   transition: all 0.2s ease-out;
   -webkit-tap-highlight-color: transparent;
-
+  ${(props) =>
+    props.svgSize &&
+    css`
+      svg {
+        width: ${props.svgSize}px;
+        height: ${props.svgSize}px;
+      }
+    `}
   &:hover {
     color: ${colors.brightPurble0};
     background: #0000;
